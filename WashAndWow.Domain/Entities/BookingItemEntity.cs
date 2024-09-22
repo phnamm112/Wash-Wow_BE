66d2feff3cf63684c21ca320709426e1f5ba2444
NemissU <namphse171651@fpt.edu.vn>
2024-09-23 06:49:07 +0700

    Nam: Entity building

---
+++ WashAndWow.Domain/Entities/BookingItemEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wash_Wow.Domain.Entities;
+using Wash_Wow.Domain.Entities.Base;
+
+namespace WashAndWow.Domain.Entities
+{
+    [Table("BookingItem")]
+    public class BookingItemEntity : BaseEntity
+    {
+        public required string ServicesID {  get; set; }
+        [ForeignKey(nameof(ServicesID))]
+        public virtual ShopServiceEntity ShopService { get; set; }
+        public required string BookingID {  get; set; }
+        [ForeignKey(nameof(BookingID))]
+        public virtual BookingEntity Booking { get; set; }
+    }
+}
---
+++ WashAndWow.Domain/Entities/ShopServiceEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wash_Wow.Domain.Entities;
+using Wash_Wow.Domain.Entities.Base;
+
+namespace WashAndWow.Domain.Entities
+{
+    [Table("ShopService")]
+    public class ShopServiceEntity : BaseEntity
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required decimal PricePerKg { get; set; }
+        public required string ShopID { get; set; }
+        [ForeignKey(nameof(ShopID))]
+        public virtual LaundryShopEntity LaundryShop { get; set; }
+    }
+}
