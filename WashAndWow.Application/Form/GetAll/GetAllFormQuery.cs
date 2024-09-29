2d804f495b1e22cf639650c973953546e40304f4
NemissU <namphse171651@fpt.edu.vn>
2024-09-29 23:44:33 +0700

    Nam: GET forms

---
+++ WashAndWow.Application/Form/GetAll/GetAllFormQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wash_Wow.Application.Common.Interfaces;
+
+namespace WashAndWow.Application.Form.GetAll
+{
+    public class GetAllFormQuery : IRequest<List<FormDto>>, IQuery
+    {
+        public GetAllFormQuery()
+        {
+            
+        }
+    }
+}
---
+++ WashAndWow.Application/Form/GetAll/GetAllFormQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WashAndWow.Domain.Repositories;
+
+namespace WashAndWow.Application.Form.GetAll
+{
+    public class GetAllFormQueryHandler : IRequestHandler<GetAllFormQuery, List<FormDto>>
+    {
+        private readonly IFormRepository _formRepository;
+        private readonly IFormTemplateRepository _formTemplateRepository;
+        private readonly IFormTemplateContentRepository _formTemplateContentRepository;
+        private readonly IFormFieldValueRepository _formFieldValueRepository;
+        private readonly IMapper _mapper;
+        public GetAllFormQueryHandler(IFormRepository formRepository
+            , IFormTemplateRepository formTemplateRepository
+            , IFormTemplateContentRepository formTemplateContentRepository
+            , IFormFieldValueRepository formFieldValueRepository
+            , IMapper mapper)
+        {
+            _formRepository = formRepository;
+            _formTemplateRepository = formTemplateRepository;
+            _formTemplateContentRepository = formTemplateContentRepository;
+            _formFieldValueRepository = formFieldValueRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<FormDto>> Handle(GetAllFormQuery request, CancellationToken cancellationToken)
+        {
+            var contents = await _formTemplateContentRepository.FindAllToDictionaryAsync(x => x.DeletedAt == null, x => x.ID, x => x.Name, cancellationToken);
+            var result = await _formRepository.FindAllAsync(x => x.DeletedAt == null, cancellationToken);
+            var title = await _formTemplateRepository.FindAllToDictionaryAsync(x => x.DeletedAt == null, x => x.ID, x => x.Name, cancellationToken);
+            return result.MapToFormDtoList(_mapper, title, contents);
+        }
+    }
+}
