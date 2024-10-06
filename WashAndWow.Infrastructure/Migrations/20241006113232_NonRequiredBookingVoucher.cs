06d133210afdfdfa10459d3774f3223b262ac242
NemissU <namphse171651@fpt.edu.vn>
2024-10-06 18:33:02 +0700

    Nam: fixed relationship

---
+++ WashAndWow.Infrastructure/Migrations/20241006113232_NonRequiredBookingVoucher.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WashAndWow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NonRequiredBookingVoucher : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Voucher_VoucherID",
+                table: "Booking");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VoucherID",
+                table: "Booking",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_Voucher_VoucherID",
+                table: "Booking",
+                column: "VoucherID",
+                principalTable: "Voucher",
+                principalColumn: "ID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Voucher_VoucherID",
+                table: "Booking");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VoucherID",
+                table: "Booking",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_Voucher_VoucherID",
+                table: "Booking",
+                column: "VoucherID",
+                principalTable: "Voucher",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
