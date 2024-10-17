25a15953441e964a272825bfe00762c4be4c3cad
nhanntse173258 <nhanntse173258@fpt.edu.vn>
2024-10-17 18:12:58 +0700

    add case comfirm and reject of booking when update status and send noti

--- WashAndWow.Application/Booking/UpdateStatus/UpdateBookingStatusCommand.cs
+++ WashAndWow.Application/Booking/UpdateStatus/UpdateBookingStatusCommand.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Wash_Wow.Application.Common.Interfaces;
 using static Wash_Wow.Domain.Enums.Enums;
@@ -12,15 +13,14 @@ namespace WashAndWow.Application.Booking.UpdateStatus
     public class UpdateBookingStatusCommand : IRequest<string>, ICommand
     {
         public string BookingId { get; set; }
-        public BookingStatus NewStatus { get; set; }
-        public UpdateBookingStatusCommand()
-        {
+        public object NewStatus { get; set; }
 
-        }
-        public UpdateBookingStatusCommand(string bookingId, BookingStatus bookingStatus)
+        public UpdateBookingStatusCommand() { }
+
+        public UpdateBookingStatusCommand(string bookingId, object newStatus)
         {
             BookingId = bookingId;
-            NewStatus = bookingStatus;
+            NewStatus = newStatus;
         }
     }
 }
